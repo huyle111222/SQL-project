c9607a812188df71fa8a80c70f3c884681135f94
huyle111222 <129925648+huyle111222@users.noreply.github.com>
2024-10-01 23:10:45 +0700

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,5 +1,5 @@
 ﻿use Northwind
--- Câu 1 Xuất danh sách các nhà cung cấp (gồm Id, CompanyName, ContactName, City, Country, Phone)
+-- Xuất danh sách các nhà cung cấp (gồm Id, CompanyName, ContactName, City, Country, Phone)
 --kèm theo giá min và max của các sản phẩm mà nhà cung cấp đó cung cấp. Có sắp xếp theo thứ tự Id của nhà cung cấp 
 
 
@@ -11,7 +11,7 @@ on S.Id=P.SupplierId
 group by S.Id,S.CompanyName,S.City,S.Country,S.Phone,P.SupplierId
 order by P.SupplierId 
 
--- Câu 2 Cũng câu trên nhưng chỉ xuất danh sách nhà cung cấp có sự khác biệt giá (max – min) 
+-- Cũng câu trên nhưng chỉ xuất danh sách nhà cung cấp có sự khác biệt giá (max – min) 
 --không quá lớn (<=30).
 
 select S.Id,S.CompanyName,S.City,S.Country,S.Phone,P.SupplierId,
@@ -23,7 +23,7 @@ group by S.Id,S.CompanyName,S.City,S.Country,S.Phone,P.SupplierId
 having (Max(P.UnitPrice) - Min(P.UnitPrice)) <=30
 order by P.SupplierId 
 
---Câu 3 Xuất danh sách các hóa đơn (Id, OrderNumber, OrderDate) 
+--Xuất danh sách các hóa đơn (Id, OrderNumber, OrderDate) 
 --kèm theo tổng giá chi trả (UnitPrice*Quantity) cho hóa đơn đó, bên cạnh đó có cột Description là “VIP” 
 --nếu tổng giá lớn hơn 1500 và “Normal” nếu tổng giá nhỏ hơn 1500
 
@@ -40,7 +40,7 @@ from OrderItem as I , [Order] as O
 where I.Quantity*I.UnitPrice < 1500
 group by O.Id,O.OrderDate,O.OrderNumber,I.Quantity,I.UnitPrice
 
---câu 4 Xuất danh sách những hóa đơn (Id, OrderNumber, OrderDate) trong tháng 7 
+--Xuất danh sách những hóa đơn (Id, OrderNumber, OrderDate) trong tháng 7 
 --nhưng phải ngoại trừ ra những hóa đơn từ khách hàng France. 
 
 select  O.Id,O.OrderDate,O.OrderNumber,O.CustomerId,C.Country,month(O.OrderDate) as 'month'
@@ -53,7 +53,7 @@ select  O.Id,O.OrderDate,O.OrderNumber,O.CustomerId,C.Country,month(O.OrderDate)
 from [Order] as O,Customer as C
 WHERE Country = 'France'
 
--- Câu 5 Xuất danh sách những hóa đơn (Id, OrderNumber, OrderDate, TotalAmount)  nào có TotalAmount 
+-- Xuất danh sách những hóa đơn (Id, OrderNumber, OrderDate, TotalAmount)  nào có TotalAmount 
 --nằm trong top 5 các hóa đơn. 
 
 select Id, OrderNumber, OrderDate, TotalAmount
